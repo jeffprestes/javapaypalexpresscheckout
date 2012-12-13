83d21162126bfa4dfabf1f1605683c7fdbde7d47
aalonso <aalonso@L-SJN-00535936.corp.ebay.com>
2012-12-13 08:15:59 -0200

    Todos parametros de getExpressCheckoutDetails antes do Do, forem refeitos e propriamente recebidos pela Viewer e inseridos no Modelo.

--- src/main/java/br/com/paypalbrasil/x/controller/ExpressCheckoutController.java
+++ src/main/java/br/com/paypalbrasil/x/controller/ExpressCheckoutController.java
@@ -115,6 +115,62 @@ public class ExpressCheckoutController extends HttpServlet {
                     logger.info("VERSION=" + resp.getVersao());
                     rd.forward(request, response);
                 }
+                //DoExpressCheckoutPayment
+                if ("DOEXPRESSCHECKOUTPAYMENT".equalsIgnoreCase(request.getParameter("METHOD")))    {
+                
+                    Credenciais cr = new Credenciais(request.getParameter("USER"), request.getParameter("PWD"), request.getParameter("SIGNATURE"));
+
+                    PagamentoSimples ps = new PagamentoSimples(cr);
+                    SetExpressCheckoutResposta resp = ps.setExpressCheckout(request.getParameterMap());
+
+                    if (resp.getCabecalho().getAck()==CodigoACK.Success)     {
+                        if (resp.getToken()!= null)     {
+                            StringBuilder url = new StringBuilder();
+                            if (request.getParameter("NAOENVIAR_ENDPOINT").contains("sandbox")) {
+                                url.append("https://www.sandbox.paypal.com");
+                            }   else    {
+                                url.append("https://www.paypal.com");
+                            }
+
+                            url.append("/br/cgi-bin/webscr?cmd=");
+                            url.append(request.getParameter("NAOENVIAR_TIPOCHECKOUT"));
+                            /*
+                            if (request.getParameter("NAOENVIAR_TIPOCHECKOUT").contains("_express-checkout"))  {
+                                url.append("_expresscheckout");
+                            }   else if (request.getParameter("NAOENVIAR_TIPOCHECKOUT").contains("_mobile-express-checkout"))  {
+                                url.append("_mobile-express-checkout");
+                            }
+                            */
+
+                            if (request.getParameter("NAOENVIAR_TIPOFLUXO").contains("commit"))  {
+                                url.append("&useraction=commit");
+                            }   
+
+                            url.append("&token=");
+                            url.append(resp.getToken());
+
+                            //Armazena o token na Sessao
+                            HttpSession sessao = request.getSession();
+                            sessao.setAttribute("token", resp.getToken());
+
+                            if (request.getParameter("NAOENVIAR_OPCREDIRECT").equals("0"))  {
+                                logger.info("- Redirecionando o usuario para: " + url.toString());
+                                response.sendRedirect(url.toString());
+                            }   else    {
+                                request.setAttribute("resposta", resp);
+                                request.setAttribute("url", url.toString());
+                                RequestDispatcher rd = request.getRequestDispatcher("expcheckout_retorno_setEC.jsp");
+                                rd.forward(request, response);
+                            }
+
+                        }
+
+                    }   else if (resp.getCabecalho().getAck()==CodigoACK.Failure)   {
+                        request.setAttribute("resposta", resp);
+                        RequestDispatcher rd = request.getRequestDispatcher("/expcheckout_erro_chamada.jsp");
+                        rd.forward(request, response);
+                    }
+                }
             }
             
         } finally {            
---
+++ src/main/webapp/expcheckout_doexpcheckout.jsp
@@ -0,0 +1,75 @@
+<%-- 
+    Document   : expcheckout_doexpcheckout.jsp
+    Created on : 05/06/2012, 14:36:18
+    Author     : aalonso
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>PayPal Java Testes - Express Checkout - Efetuar Transação Express Checkout</title>
+    </head>
+    <body>
+        <h1>PayPal Java Testes - Express Checkout - Efetuar Transação Express Checkout</h1>
+        <table>
+            <form action='<c:out value="${pageContext.servletContext.contextPath}" />/ExpressCheckoutController' method="POST">  
+            <tr>
+                <td>URL do EndPoint: </td>
+                <td>
+                    <select id="NAOENVIAR_ENDPOINT" name="NAOENVIAR_ENDPOINT">
+                        <option value="https://api-3t.sandbox.paypal.com/nvp">Sandbox - api-3t.sandbox.paypal.com/nvp</option>
+                        <option value="https://api-3t.paylpal.com/nvp">Produção - api-3t.paypal.com</option>
+                    </select>
+                </td>
+            </tr>    
+            <tr>
+                <td>Metodo (METHOD): </td>
+                <td>
+                    <input type="text" value="DoExpressCheckoutPayment" size="60" name="METHOD" id="METHOD" />
+                </td>
+            </tr>
+            <tr>
+                <td>Usuario (USER): </td>
+                <td>
+                    <input type="text" value="jeffprestes_api1.gmail.com" size="60" name="USER" id="USER" />
+                </td>
+            </tr>
+            <tr>
+                <td>Senha (PWD): </td>
+                <td>
+                    <input type="text" value="3SZKBPGTTLMVQ66B" size="60" name="PWD" id="PWD" />
+                </td>
+            </tr>
+            <tr>
+                <td>Assinatura (SIGNATURE): </td>
+                <td>
+                    <input type="text" value="ATutHe2f5sqGdXTHx0Gdsr3F4EnaAPbdeidoBxokDfl3cK.mR1XmuSHH" size="60" name="SIGNATURE" id="SIGNATURE" />
+                </td>
+            </tr>
+            <tr>
+                <td>Versao (VERSION - Para saber a ultima versão vá a pagina paypal.com e veja no fonte da pagina os 3 primeiros digitos do campo webversion) : </td>
+                <td>
+                    <input type="text" value="91.0" size="10" name="VERSION" id="VERSION" />
+                </td>
+            </tr>
+            <tr>
+                <td>Token (TOKEN): </td>
+                <td>
+                    <input type="text" value='<c:out value="${sessionScope.token}" />' size="60" name="TOKEN" id="TOKEN" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">&nbsp;</td>
+            </tr>
+            <tr>
+                <td colspan="2" style="vertical-align: middle; text-align: center">
+                    <input type="submit" id="NAOENVIAR_Submit" name="NAOENVIAR_Submit" value="Submit" />
+                </td>
+            </tr>
+            </form>
+        </table>
+    </body>
+</html>
