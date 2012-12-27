fc0e81a91bd8d42496c5e17984d7d2ea6b2adb45
aalonso <aalonso@L-SJN-00535936.corp.ebay.com>
2012-12-27 11:19:08 -0200

    Inicio de pagina de teste com Instant Update

--- src/main/java/br/com/paypalbrasil/x/ajax/FreteServlet.java
+++ src/main/java/br/com/paypalbrasil/x/ajax/FreteServlet.java
@@ -37,7 +37,7 @@ public class FreteServlet extends HttpServlet {
                     Enumeration<String> name = request.getParameterNames();
                     while(name.hasMoreElements()){
                     String temp = name.nextElement().toString();
-                    logger.info(temp+" = "+request.getParameterValues(temp));
+                    logger.info(temp+" = "+request.getParameterValues(temp).toString());
                     }
                     
                     
