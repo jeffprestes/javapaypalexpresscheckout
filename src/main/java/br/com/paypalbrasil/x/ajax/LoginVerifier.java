e7b0418c0b344f58a9a90fcf6877e5c35d12d19b
aalonso <aalonso@L-SJN-00535936.corp.ebay.com>
2013-02-01 15:53:15 -0200

    Criacao de pagina de login

--- src/main/java/br/com/paypalbrasil/x/ajax/LoginVerifier.java
+++ src/main/java/br/com/paypalbrasil/x/ajax/LoginVerifier.java
@@ -42,7 +42,7 @@ public class LoginVerifier extends HttpServlet {
             {
                 rd = request.getRequestDispatcher("index.jsp");
             }else{
-                rd = request.getRequestDispatcher("index2.jsp");
+                rd = request.getRequestDispatcher("auth.jsp");
                 HttpSession sessao = request.getSession();
                 sessao.setAttribute("msgErro", "Login invalido, tente novamente.");
             }
